77c22d297f5916548c24802f52e71cf8a12473b1
David M. Rogers <predictivestatmech@gmail.com>
2021-06-12 18:54:49 -0400

    Added simulate()

--- cheat.cc
+++ cheat.cc
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <iostream>
+#include <algorithm> /* std::swap */
 #include <string>
 
 using real = double;
@@ -118,8 +119,74 @@ Params parse_args(int argc, char *argv[]) {
     return ret;
 }
 
+/* Class storing the energy as a function of position
+ * at a single time.  The internal representation uses a grid
+ * with N intervals and spacing dx.
+ *
+ * The grid values are stored at the end-points of each
+ * interval, making the internal vector size N+1.
+ *
+ */
+struct Energy {
+    const int N;
+    const real dx;
+    std::vector<real> u, u_last;
+
+    Energy(const Params &p)
+      : N(p.Nx), dx(p.dx)
+      , u(N+1, 1.0)
+      , u_last(N+1) {}
+
+    /* Update the solution, u and u_last */
+    void step(real alpha, real dt) {
+        // time-constant for updating
+        real k = alpha*dt / (dx*dx);
+        // Start by swapping u and last
+        std::swap(u, u_last);
+        // Now paint new solution into u
+
+        // update (N-1) interior points
+        // partial u/partial t = alpha partial^2 u/partial x^2
+        // \Delta u = (dt*alpha/dx^2) D^2 u
+        for(int i=1; i<N; i++) {
+            u[i] = u_last[i] + k*(u_last[i-1]-2*u_last[i]+u_last[i+1]);
+        }
+        u[0] = u_last[0];
+        u[N] = u_last[N];
+    }
+    void set_bc(const real u0, const real u1) {
+        u[0] = u0;
+        u[N] = u1;
+    }
+    real en() {
+        // C++17 parallel reduce [#include<numeric>]
+        //real ans = std::reduce(std::execution::par, u.cbegin(), u.cend());
+        real ans = 0.5*(u[0]+u[N]);
+        for(int i=1; i<N; i++) {
+            ans += u[i];
+        }
+        return ans*dx;
+    }
+};
+
+void simulate(Params &p) {
+    Energy E(p);
+
+    std::cout << "Time Energy\n";
+    for(int n=0; n<p.Nt; n++) {
+        E.set_bc(p.u0, p.u1);
+        E.step(p.alpha, p.dt);
+        if(n%10 == 0)
+            std::cout << (n+1)*p.dt << " "
+                      << E.en() << std::endl;
+    }
+}
+
 int main(int argc, char *argv[]) {
     Params p = parse_args(argc, argv);
     p.show();
+
+    simulate(p);
+
     return 0;
 }
